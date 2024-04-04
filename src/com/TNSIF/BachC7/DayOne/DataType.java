18b038d12876ebcbf9677fceb04c00e492751cbe
user <user@DESKTOP-7MV7FE8>
2024-04-04 21:26:55 +0530

    second	commit

---
+++ src/com/TNSIF/BachC7/DayOne/DataType.java
@@ -0,0 +1,40 @@
+package com.TNSIF.BachC7.DayOne;
+
+public class DataType {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		int		value1=9/2;
+		float	value2=101/61;
+		double	value3=10d/6d;
+		
+		System.out.println("value1:"+value1);
+		System.out.println("value2:"+value2);
+		System.out.println("value3:"+value3);
+		
+		
+		
+		//int	for=13;
+		//System.out.println("value3:"+for);///invalid	it	is	a	keyword
+		
+		int	For=13;
+		System.out.println("value3:"+For);
+		
+		
+		String	StudentName	="Hemapriya";
+		System.out.println("camelcase:"+StudentName);
+		
+		//int	@number=120;
+		
+		int	$num=10;
+		System.out.println("Dollar	symbol:"+$num);
+		
+	}
+
+}
+
+
+
+
---
+++ src/com/TNSIF/BatchC37/DayThree/DecisionStatements.java
@@ -0,0 +1,57 @@
+package com.TNSIF.BatchC37.DayThree;
+
+public class DecisionStatements {
+
+	public static void main(String[] args)
+	
+	{
+		//if	statement
+		int	mark=92;
+		if(mark>=50)
+		{
+			System.out.println("pass");
+		}
+		
+		
+		
+		//if	else	statement
+		int	age=11;
+		if(age>=18)
+			{
+				System.out.println("Eligible");
+				
+			}
+			
+		else
+			{
+				System.out.println("Not	Eligible");
+		
+			}
+		
+		
+		
+		//nested	if
+			float	cgpa=9.5f;
+			if(cgpa>=6.5)
+			{
+				if(cgpa>=8.5)
+				{
+					System.out.println("Excellent");
+				}
+				else
+				{
+					System.out.println("average");
+				}
+			}
+			else
+				{
+					System.out.println("very	low");
+				}
+			
+			}
+	
+		
+
+	}
+
+
